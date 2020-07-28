f1577ccd77dd4fb02ee420023e70734bee8736f9
Anjana <sinhaanjana32@gmail.com>
2020-07-28 11:13:27 +0300

    add client

--- server/index.js
+++ server/index.js
@@ -50,19 +50,19 @@ app.use('/api/users', require('./routes/users'));
 //app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
 
   // Set static folder   
   // All the javascript and css files will be read and served from this folder
-  app.use(express.static("client/build"));
-}
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
   // index.html for all page routes    html or routing and naviagtion
-  app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  app.get('/*',(req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
 });
 
 
-
+}
 
 
 const port = process.env.PORT || 8080
